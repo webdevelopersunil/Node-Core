def99f8940de4364d64dbedf5df9ca239e81b18d
webdevelopersunil <he.kingsmen@gmail.com>
2023-08-31 18:22:06 +0530

    Extending To Multi Pages

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ dir : basichttpserver, being used the file system for print html file into the b
 # Nodemon
 - Is a package which monitoring the changes into the files except the html file which is being rendering via file system readFile functions.
 - It's auto restart the server once chnages detected.
+
+# Extended to Multi Pages through Switch Routes
+- being used res.url for identify the route and Switch case for multi page paths.
+- for rendering page data used the fs(file system) function readFile.
--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -5,22 +5,50 @@ const port = 8000;
 const fs = require('fs');
 
 function requestHandler(req, res){
+
     console.log(req.url);
 
     res.writeHead(200, {'content-type': 'text/html'});
 
+    // 1. Serving a Simple response to the Browser
     // res.end("<h1>Stop Here</h1>");
-    // Reading the file through the file system method
-    fs.readFile('./index.html', function(err, data){
+
+    // 2. Reading the file through the file system method
+    // fs.readFile('./index.html', function(err, data){
+    //     if(err){
+    //         console.log(err);
+    //         return res.end('<h1>Error!</h1>')
+    //     }
+
+    //     return res.end(data);
+    // });
+
+    // 3. Extending to MultiPages access using Switch Cases
+
+    let filePath;
+    
+    switch(req.url){
+        case '/':
+            filePath = './index.html';
+            break;
+        case '/profile':
+            filePath = './profile.html';
+            break;
+        default:
+            filePath = './404.html';
+            break;
+    }
+
+    fs.readFile(filePath, function(err, data){
 
         if(err){
-            console.log(err);
-            return res.end('<h1>Error!</h1>')
+            console.log('err', err);
+            return res.end('<h1>Error!</h1>');
         }
 
         return res.end(data);
 
-    });
+    })
 
 }
 
