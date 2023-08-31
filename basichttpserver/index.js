ccfd57db9dbc98d7f8410931e329abd09b960d99
webdevelopersunil <he.kingsmen@gmail.com>
2023-08-31 17:17:21 +0530

    writeHead : for content type and type to the document

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -3,8 +3,8 @@ const port = 8000;
 
 function requestHandler(req, res){
     console.log(req.url);
-
-    res.end("Stop Here");
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end("<h1>Stop Here</h1>");
 }
 
 const server = http.createServer(requestHandler);
