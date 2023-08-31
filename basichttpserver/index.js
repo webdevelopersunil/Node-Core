70d7eb8e3f56450b127366243326a78edda5cdc3
webdevelopersunil <he.kingsmen@gmail.com>
2023-08-31 17:29:30 +0530

    Reading and Serving Html File

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,10 +1,28 @@
 const http = require('http');
 const port = 8000;
 
+// importing the file system to read the file
+const fs = require('fs');
+
 function requestHandler(req, res){
     console.log(req.url);
+
     res.writeHead(200, {'content-type': 'text/html'});
-    res.end("<h1>Stop Here</h1>");
+
+    
+    // res.end("<h1>Stop Here</h1>");
+    // Reading the file through the file system method
+    fs.readFile('./index.html', function(err, data){
+
+        if(err){
+            console.log(err);
+            return res.end('<h1>Error!</h1>')
+        }
+
+        return res.end(data);
+
+    });
+
 }
 
 const server = http.createServer(requestHandler);
