f08c74fdb4a274d0be4808c063ed9660bb6b9ebe
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-06 00:50:14 +0530

    Render the homepage using ejs template engine

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+const path = require('path');
+
+app.set('view engine', 'views');
+
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.static('./assets'));
+
+app.use(express.urlencoded);
 
 
+
+
+app.use('/', require('./routes/index'));
 
 
 
