67eba331b16cccca61c9ac08bd8788dd5373acdf
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-06 15:43:54 +0530

    Established Connection with Mongoose and Created Model for Todo

---
+++ todo-app/config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// connected to the database
+mongoose.connect("mongodb://localhost/todos");
+
+// accesing the database Acquire the connection to check if it is successfull
+const db = mongoose.connection;
+
+// Error
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
+// If conection is success
+db.once('open', function(){
+    console.log('Successfully connected to the database');
+});
